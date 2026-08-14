c9f89e2d4d58adc171234e8ceb4d15adcb77f5ca
NikitaUmpelev <umpelevnikita98@gmail.com>
2026-08-14 16:32:17 +0300

    Update mpv.user.js

--- mpv.user.js
+++ mpv.user.js
@@ -8,8 +8,8 @@
 // @grant        GM_getValue
 // @grant        GM_xmlhttpRequest
 // @connect      api.npoint.io
-// @updateURL    https://raw.githubusercontent.com/NikitaUmpelev/multi-point-assistant/refs/heads/main/mpv.user.js
-// @downloadURL  https://raw.githubusercontent.com/NikitaUmpelev/multi-point-assistant/refs/heads/main/mpv.user.js
+// @updateURL    https://raw.githubusercontent.com/NikitaUmpelev/multi-point-assistant/main/mpv.user.js
+// @downloadURL  https://raw.githubusercontent.com/NikitaUmpelev/multi-point-assistant/main/mpv.user.js
 // ==/UserScript==
 
 (function() {
